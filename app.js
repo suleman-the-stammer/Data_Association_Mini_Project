89aa8c663c5e66b09e4fc195e7d8fd0ad147d775
stammer1 <stammer786@gmail.com>
2024-12-06 06:04:00 -0800

    creating_Ui

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const userModel = require("./models/user")
+
+
+app.set("view engine" , "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: ture}));
+app.use(cookieParser());
+
+
+app.get('/' , function(req ,res){
+    res.render("index");
+})
+
+app.post('/register' ,async function(req, res){
+    let {username , name , email , age , password } = req.body;
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send("Email Already Registered")
+
+})
+app.listen(3000);
